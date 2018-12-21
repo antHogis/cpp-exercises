181a8a96dbd641e8d8341a6d2d20f4a74af9f17d
Anton H <antonjhoglund@hotmail.com>
2018-12-21 02:58:51 +0200

    Add exercise 011

---
+++ 011-hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string date = "2018-12-21";
+    string name = "Anton Höglund";
+    int height = 181;
+
+    cout << "The date is " << date << '\n';
+    cout << "Hello, I'm " << name << '\n';
+    cout << "I'm " << height << " cm tall\n"; 
+}
